da78cdf8d2b0edf9c0680853b43f4b9802d772db
wjddu <wjddu@JYG>
2025-07-16 15:46:56 +0900

    DTO 추가

---
+++ KOSA_Team_Bread/Team_Bread/src/model/dto/Category.java
@@ -0,0 +1,19 @@
+package model.dto;
+import lombok.*;
+
+@Data
+@Builder
+public class Category {
+
+	// 카테고리 ID
+	private Integer categoryId;
+	
+	// 카테고리 부모 ID
+	private Integer parentId;
+	
+	// 카테고리 명
+	private String categoryName;
+	
+	// 카테고리 레벨
+	private Integer level;
+}
